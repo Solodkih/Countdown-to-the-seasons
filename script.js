208c28a1507eca61af3e9cc29edd7e55329f7446
Solodkih <solodkih_v_p@mail.ru>
2022-02-21 17:41:42 +0600

    create input end main function

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ window.onload = function () {
   };
 
   const inputDate = {
-    year: document.getElementById("year"),
-    month: document.getElementById("month"),
-    days: document.getElementById("days"),
-    hours: document.getElementById("hours"),
-    minutes: document.getElementById("minutes"),
-    seconds: document.getElementById("seconds"),
+    year: document.getElementById("input_year"),
+    month: document.getElementById("input_month"),
+    days: document.getElementById("input_days"),
+    hours: document.getElementById("input_hours"),
+    minutes: document.getElementById("input_minutes"),
+    seconds: document.getElementById("input_seconds"),
   };
 
   let timeStart = new Date();
@@ -23,16 +23,9 @@ window.onload = function () {
 
   function setData(millsec) {
     date.days.innerHTML = Math.floor(millsec / 1000 / 60 / 60 / 24);
-    
-    console.log(Math.floor(millsec / 1000 / 60 / 60 / 24));
-
-
-    date.hours.innerHTML = '22'; //Math.floor(millsec / 1000 / 60 / 60) % 24;
-    //console.log(Math.floor(millsec / 1000 / 60 / 60) % 24);
+    date.hours.innerHTML = Math.floor(millsec / 1000 / 60 / 60) % 24;
     date.minutes.innerHTML = Math.floor(millsec / 1000 / 60) % 60;
-    //console.log(Math.floor(millsec / 1000) % 60);
     date.seconds.innerHTML = Math.floor(millsec / 1000) % 60;
-    //console.log(Math.floor(millsec / 1000) % 60);
   }
 
   function getTimeEnd() {
@@ -46,6 +39,10 @@ window.onload = function () {
   setInterval(() => {
     timeStart = new Date();
     timeEnd = new Date(...getTimeEnd());
-    setData(diffTime(timeEnd, timeStart));
+    if (timeStart.getTime() < timeEnd.getTime()) {
+      setData(diffTime(timeEnd, timeStart));
+    } else {
+      setData(diffTime(timeStart, timeStart));
+    }
   }, 1000);
 };
